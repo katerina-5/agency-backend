ff48d74c27d7fee35e90b8a9ac72c31a35ca36fe
katerina-5 <pochta13524@gmail.com>
2019-04-06 23:07:58 +0300

    Add config for postgreSQL database

---
+++ config/postgresql.js
@@ -0,0 +1,8 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'qoxhouqs',
+    host: 'isilo.db.elephantsql.com',
+    database: 'qoxhouqs',
+    password: 'edtR-DTl2XtTW1Peun_kBmEvec-Wyejf',
+    port: 5432,
+});
