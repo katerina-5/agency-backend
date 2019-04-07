586279027c522fb8a4a351bc03b26e575e44dce3
katerina-5 <pochta13524@gmail.com>
2019-04-08 00:23:20 +0300

    Add sql queries for office categories controller

--- controllers/office_categories.js
+++ controllers/office_categories.js
@@ -9,21 +9,65 @@ module.exports = {
 }
 
 function office_category_list(request, response, next) {
-    console.log("List of office categories");
+    console.log("List of office_categories");
+
+    pool.query('SELECT * FROM office_categories', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function office_category_detail(request, response, next) {
     console.log("Office category detail");
+
+    const o_cat_id = request.params.id;
+
+    pool.query('SELECT * FROM office_categories WHERE o_cat_id = $1', [o_cat_id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function office_category_create(request, response, next) {
     console.log("Office category create");
+
+    const { title } = request.body;
+
+    pool.query('INSERT INTO office_categories(title) VALUES($1)', [title], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function office_category_update(request, response, next) {
     console.log("Office category update");
+
+    const o_cat_id = request.params.id;
+    const { title } = request.body;
+
+    pool.query('UPDATE office_categories SET title = $2 WHERE o_cat_id = $1', [o_cat_id, title], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function office_category_delete(request, response, next) {
     console.log("Office category delete");
+
+    const o_cat_id = request.params.id;
+
+    pool.query('DELETE FROM office_categories WHERE o_cat_id = $1', [o_cat_id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
