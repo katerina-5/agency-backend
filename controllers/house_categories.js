c28c425a0d2942c80aa1eef08a7f039c9ce5162d
katerina-5 <pochta13524@gmail.com>
2019-04-08 00:22:54 +0300

    Add sql queries for house categories controller

--- controllers/house_categories.js
+++ controllers/house_categories.js
@@ -10,20 +10,64 @@ module.exports = {
 
 function house_category_list(request, response, next) {
     console.log("List of house_categories");
+
+    pool.query('SELECT * FROM house_categories', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function house_category_detail(request, response, next) {
     console.log("House category detail");
+
+    const h_cat_id = request.params.id;
+
+    pool.query('SELECT * FROM house_categories WHERE h_cat_id = $1', [h_cat_id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function house_category_create(request, response, next) {
     console.log("House category create");
+
+    const { title } = request.body;
+
+    pool.query('INSERT INTO house_categories(title) VALUES($1)', [title], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function house_category_update(request, response, next) {
     console.log("House category update");
+
+    const h_cat_id = request.params.id;
+    const { title } = request.body;
+
+    pool.query('UPDATE house_categories SET title = $2 WHERE h_cat_id = $1', [h_cat_id, title], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
 
 function house_category_delete(request, response, next) {
     console.log("House category delete");
+
+    const h_cat_id = request.params.id;
+
+    pool.query('DELETE FROM house_categories WHERE h_cat_id = $1', [h_cat_id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
 }
